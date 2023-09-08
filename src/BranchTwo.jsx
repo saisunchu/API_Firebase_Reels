4dd0016b2dff436413ae754e54cc60203e51e5a3
saiKumar <saikumarsunchu6@gmail.com>
2023-09-08 14:07:16 +0530

    added BranchTwo Screen

---
+++ src/BranchTwo.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BranchTwo = () => {
+  return (
+    <View>
+      <Text>BranchTwo</Text>
+    </View>
+  )
+}
+
+export default BranchTwo
